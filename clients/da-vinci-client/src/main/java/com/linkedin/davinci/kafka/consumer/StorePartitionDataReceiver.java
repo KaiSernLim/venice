8deb9e23612e1034125f7f65fbdc289d09851ac9
Kai-Sern Lim <kailim@linkedin.com>
2024-10-19 15:33:07 -0700

    Refactored `shouldProduceInBatch()` from `StoreIngestionTask` into `StorePartitionDataReceiver`. 🧹

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/StorePartitionDataReceiver.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/StorePartitionDataReceiver.java
@@ -203,7 +203,7 @@ public class StorePartitionDataReceiver
      */
     records = validateAndFilterOutDuplicateMessagesFromLeaderTopic(records, kafkaUrl, topicPartition);
 
-    if (storeIngestionTask.shouldProduceInBatch(records)) {
+    if (shouldProduceInBatch(records)) {
       produceToStoreBufferServiceOrKafkaInBatch(
           records,
           topicPartition,
@@ -335,6 +335,12 @@ public class StorePartitionDataReceiver
         topicPartition.getPartitionNumber(),
         elapsedTimeForPuttingIntoQueue,
         beforeProcessingBatchRecordsTimestampMs);
+  }
+
+  private boolean shouldProduceInBatch(Iterable<PubSubMessage<KafkaKey, KafkaMessageEnvelope, Long>> records) {
+    return (storeIngestionTask.isActiveActiveReplicationEnabled() || storeIngestionTask.isTransientRecordBufferUsed())
+        && storeIngestionTask.getServerConfig().isAAWCWorkloadParallelProcessingEnabled()
+        && IngestionBatchProcessor.isAllMessagesFromRTTopic(records);
   }
 
   private void updateMetricsAndEnforceQuota(
